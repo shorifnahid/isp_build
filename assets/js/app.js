a4773888944a13d7b4a3eb1fb21ebfcdeb280067
mdnahid26 <naimulnihal2696@gmail.com>
2021-10-04 09:29:28 +0600

    3rd upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,9 +19,40 @@ $(function(){
         autoplaySpeed:4000,
         dots:true,
         dotsClass:'banner_dots',
+        
+    })
+    //project slick
+    $('.project_content').slick({
+        slidesToShow:5,
+        prevArrow:'<i class="fas fa-chevron-left project_arrow project_left"></i>',
+        nextArrow:'<i class="fas fa-chevron-right project_arrow project_right"></i>',
     })
     //jaralex
     $('.jarallax').jarallax({
         speed: 0.8
     });
+
+    //
+ 
+        $('#bar1').barfiller({barColor:'#FFBC13'});
+        $('#bar2').barfiller({barColor:'#FFBC13'});
+        $('#bar3').barfiller({ barColor: '#FFBC13' });
+        $('#bar4').barfiller({ barColor: '#FFBC13'});
+        $('#bar5').barfiller({barColor:'#FFBC13'});
+        $('#bar6').barfiller({barColor:"#FFBC13"});
+        $('#bar7').barfiller({barColor:"#FFBC13"});
+    
+    //
+    $('.testimonial_slick').slick({
+        slidesToShow:3,
+        arrows:false,
+        infinite:false,
+    })
+
+    //
+    $('.clinent_slick').slick({
+        slidesToShow:5,
+        arrows:false,
+    })
+
 })
